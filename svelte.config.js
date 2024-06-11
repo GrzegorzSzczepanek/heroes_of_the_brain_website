8bf24bbc009f7ea6b338c31b6b9ee117c7ffbab0
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2024-06-11 15:59:48 +0200

    asd

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-		target: '#svelte',
+		adapter: adapter({
+		  // default options are shown
+		  pages: 'build',
+		  assets: 'build',
+		  fallback: null
+		}),
 		paths: {
 		  base: process.env.NODE_ENV === 'production' ? '/heroes_of_the_brain_website' : ''
 		}
