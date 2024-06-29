28007694d479b5203f4cd54f8413c657706d5ad2
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2024-06-29 21:14:39 +0200

    xds

--- src/lib/components/Category.svelte
+++ src/lib/components/Category.svelte
@@ -26,7 +26,7 @@
   });
 </script>
 
-<div class="bg-gray-800 border border-purple-600 rounded-lg w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-6 text-white transform transition-transform duration-500 hover:scale-105 px-5">
+<div class="bg-gray-800 border border-purple-600 rounded-lg w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-6 text-white transform transition-transform duration-500 hover:scale-105 px-5 mx-8">
   <div class="bg-purple-600 text-center text-lg md:text-xl lg:text-2xl font-bold py-2 rounded-t-md">{currentCategory.title}</div>
   <div class="bg-teal-700 text-center text-sm md:text-base lg:text-lg font-bold py-1 rounded-b-md mb-4">OPEN TASKS</div>
   <div class="text-center text-sm md:text-base lg:text-lg font-bold mb-2">GUARANTEED PRIZE POOL</div>
