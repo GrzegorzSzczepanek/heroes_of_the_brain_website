6e8067ce40e4c71ad1f0c06ad3657ad9c5d07e28
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2024-10-01 13:59:23 +0200

    fixed translations

--- src/lib/components/CategorySection.svelte
+++ src/lib/components/CategorySection.svelte
@@ -1,8 +1,10 @@
 <script>
+  import { derived } from "svelte/store";
   import Category from "./Category.svelte";
   import UnderlinedHeader from "./UnderlinedHeader.svelte";
+  import { isPolish } from "../../stores/languageStore";
 
-  let categories = [
+  let categoriesPL = [
     {
       title: "Neurohackathon – Social Impact Challenge",
       description:
@@ -14,13 +16,35 @@
         "To wyzwanie Neurohackathonu 'Heroes of the Brain' koncentruje się na obszarze ENTERTAINMENT, czyli nowoczesnej rozrywce. W dzisiejszym świecie pełnym doznań tworzenie nowych form spędzania wolnego czasu jest coraz bardziej wymagające. Uczestnicy będą musieli wykazać się kreatywnością, aby sprostać oczekiwaniom i dostarczyć niezapomnianych przeżyć. Tematyka obejmuje futurystyczne gry, łamigłówki oraz inne formy rozrywki."
     }
   ];
+
+  let categoriesEN = [
+    {
+      title: "Neurohackathon – Social Impact Challenge",
+      description:
+        "The 'Social Impact' challenge focuses on making positive social changes through innovative solutions. Participants will create projects that improve everyday life in an extraordinary way. This category promotes unconventional thinking, creativity, and actions aimed at helping others. Neurohackathon is a coding marathon, brainstorming, and collaboration leading to the creation of innovative projects."
+    },
+    {
+      title: "Neurohackathon – Entertainment Challenge",
+      description:
+        "The 'Heroes of the Brain' Neurohackathon's challenge focuses on the ENTERTAINMENT area, modern entertainment. In today's world full of experiences, creating new forms of spending free time is becoming increasingly demanding. Participants will have to show creativity to meet expectations and provide unforgettable experiences. The topics include futuristic games, puzzles, and other forms of entertainment."
+    }
+  ];
+  
+    let categories = derived(isPolish, $isPolish => $isPolish ? categoriesPL : categoriesEN);
+
+    let currentCategories;
+
+    categories.subscribe(value => {
+      currentCategories = value;
+    });
+
 </script>
 
 <section class="flex flex-col items-center justify-center gap-6 py-10 bg-gray-900 text-white" id="categories">
   <UnderlinedHeader title="Kategorie" />
 
   <section class="flex flex-wrap items-start justify-center gap-6 py-10">
-    {#each categories as category}
+    {#each currentCategories as category}
       <Category {category} />
     {/each}
   </section>
