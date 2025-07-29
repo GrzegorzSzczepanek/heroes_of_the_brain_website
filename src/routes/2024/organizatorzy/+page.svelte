5569d316f7504dd7e2f49f9f88db16624c2c24fc
DallogFheir <dallog.fheir@gmail.com>
2025-07-29 19:32:20 +0200

    fix: wrong title

--- src/routes/2024/organizatorzy/+page.svelte
+++ src/routes/2024/organizatorzy/+page.svelte
@@ -1,22 +1,21 @@
 <script>
-  import '../../../app.css';
-  import OrganizationLandingPage from '$lib/components/OrganizationLandingPage.svelte';
-  import TeamIntro from '$lib/components/TeamIntro.svelte';
-  import TeamSection from '$lib/components/Team.svelte';
-  import { fade } from 'svelte/transition';
-  
-
-
+  import "../../../app.css";
+  import OrganizationLandingPage from "$lib/components/OrganizationLandingPage.svelte";
+  import TeamIntro from "$lib/components/TeamIntro.svelte";
+  import TeamSection from "$lib/components/Team.svelte";
+  import { fade } from "svelte/transition";
 </script>
 
 <svelte:head>
-	<title>Organisators</title>
-	<meta name="description" content="Heroes of The Brain BCI Hackaton" />
-	<meta name="keywords" content="brain, bci, hackaton, heroes, brainaccess, solet, pwr, politechnika wroclawska" />
-	<meta name="author" content="Grzegorz Szczepanek" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta name="robots" content="index, follow" />
-	
+  <title>Organisers</title>
+  <meta name="description" content="Heroes of The Brain BCI Hackaton" />
+  <meta
+    name="keywords"
+    content="brain, bci, hackaton, heroes, brainaccess, solet, pwr, politechnika wroclawska"
+  />
+  <meta name="author" content="Grzegorz Szczepanek" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="robots" content="index, follow" />
 </svelte:head>
 
 <div transition:fade={{ duration: 400 }}>
