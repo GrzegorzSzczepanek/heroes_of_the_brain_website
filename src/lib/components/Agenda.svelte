79930c08ae3360f25b9cc1f4a35af38c1e54eb95
Grzesiek <grzegorzszczepanek48@gmail.com>
2025-10-16 22:02:06 +0200

    Update agenda dates and event details

--- src/lib/components/Agenda.svelte
+++ src/lib/components/Agenda.svelte
@@ -1,76 +1,77 @@
 <script>
   import AgendaDay from "./AgendaDay.svelte";
   import UnderlinedHeader from "./UnderlinedHeader.svelte";
-  import { isPolish } from '../../stores/languageStore';
-  import { derived } from 'svelte/store';
+  import { isPolish } from "../../stores/languageStore";
+  import { derived } from "svelte/store";
 
-  
   const agendaPL = [
     {
-      date: "Dzień Pierwszy",
+      date: "Dzień Pierwszy (29.11)",
       events: [
-        { time: "10:00", description: "Start rejestracji" },
-        { time: "11:30", description: "Ceremonia Otwarcia" },
-        { time: "12:00", description: "Sesja Sponsora Strategicznego - BrainAccess Hyperscanning Session" },
-        { time: "13:00", description: "Start całodniowego kodowania" },
+        { time: "09:00-11:00", description: "Rejestracja uczestników" },
+        { time: "11:00-11:30", description: "Oficjalne rozpoczęcie" },
+        { time: "11:30-12:00", description: "Prelekcja wprowadzająca" },
+        { time: "12:00", description: "Rozpoczęcie pracy" },
+        { time: "17:30", description: "Catering" },
       ],
     },
     {
-      date: "Dzień Drugi",
+      date: "Dzień Drugi (30.11)",
       events: [
-        { time: "13:00", description: "Koniec kodowania" },
-        { time: "13:00", description: "Start prezentacji" },
-        { time: "16:00", description: "Ogłoszenie wyników" },
-        { time: "16:15", description: "Ceremonia Końcowa" },
+        { time: "12:00", description: "Zakończenie prac" },
+        {
+          time: "12:00-15:30",
+          description: "Prezentacja zespołów | Dodatkowe prelekcje",
+        },
+        { time: "13:30-13:45", description: "Przerwa na kawę" },
+        { time: "15:30-16:00", description: "Ocena przez jury" },
+        {
+          time: "16:00-17:00",
+          description: "Wręczenie nagród i zakończenie imprezy",
+        },
       ],
     },
   ];
 
   const agendaEN = [
     {
-      date: "Day One",
+      date: "Day One (29.11)",
       events: [
-        { time: "10:00", description: "Start of registration" },
-        { time: "11:30", description: "Opening Ceremony" },
-        { time: "12:00", description: "Strategic Sponsor Session - BrainAccess Hyperscanning Session" },
-        { time: "13:00", description: "Start of all-day coding" },
+        { time: "09:00-11:00", description: "Participant registration" },
+        { time: "11:00-11:30", description: "Official opening" },
+        { time: "11:30-12:00", description: "Introductory lecture" },
+        { time: "12:00", description: "Start of work" },
+        { time: "17:30", description: "Catering" },
       ],
     },
     {
-      date: "Day Two",
+      date: "Day Two (30.11)",
       events: [
-        { time: "13:00", description: "End of coding" },
-        { time: "13:00", description: "Start of presentations" },
-        { time: "16:00", description: "Announcement of results" },
-        { time: "16:15", description: "Closing Ceremony" },
+        { time: "12:00", description: "End of work" },
+        {
+          time: "12:00-15:30",
+          description: "Team presentations | Additional lectures",
+        },
+        { time: "13:30-13:45", description: "Coffee break" },
+        { time: "15:30-16:00", description: "Jury evaluation" },
+        {
+          time: "16:00-17:00",
+          description: "Award ceremony and end of the event",
+        },
       ],
     },
   ];
 
-  const agenda = derived(isPolish, $isPolish => $isPolish ? agendaPL : agendaEN);
+  const agenda = derived(isPolish, ($isPolish) =>
+    $isPolish ? agendaPL : agendaEN
+  );
 
   let currentAgenda;
 
-  agenda.subscribe(value => {
+  agenda.subscribe((value) => {
     currentAgenda = value;
   });
 </script>
-
-<style>
-  .parallax {
-    background-image: url('/images/bg-photo-2.webp');
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-
-  .agenda-content {
-    margin: 0;
-    background-color: rgba(0, 0, 0, 0.7);
-    height: 100%;
-  }
-</style>
 
 <section id="agenda" class="parallax text-white">
   <div class="agenda-content items-center text-center md:w-1/1 py-20 px-5">
@@ -83,3 +84,19 @@
     </div>
   </div>
 </section>
+
+<style>
+  .parallax {
+    background-image: url("/images/bg-photo-2.webp");
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .agenda-content {
+    margin: 0;
+    background-color: rgba(0, 0, 0, 0.7);
+    height: 100%;
+  }
+</style>
