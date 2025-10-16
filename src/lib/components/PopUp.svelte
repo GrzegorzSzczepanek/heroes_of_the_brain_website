c25b657ec00c7ae2163b667fc031f70ef3fb989d
Grzesiek <grzegorzszczepanek48@gmail.com>
2025-10-17 00:12:20 +0200

    sasas

--- src/lib/components/PopUp.svelte
+++ src/lib/components/PopUp.svelte
@@ -8,7 +8,7 @@
   }
 </script>
 
-<div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"  on:click={handleClose}>
+<div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 "  on:click={handleClose}>
   <div class="bg-white p-6 rounded-lg shadow-lg relative max-w-md w-full text-black" id="popup" on:click|stopPropagation>
     <button class="absolute top-2 right-2 text-gray-600 hover:text-gray-900" on:click={handleClose}>×</button>
     <h2 class="text-xl font-bold mb-4">{title}</h2>
@@ -23,5 +23,6 @@
   #popup {
     height: 500px;
     overflow-y: scroll;
+    z-index: 10000;
   }
 </style>
